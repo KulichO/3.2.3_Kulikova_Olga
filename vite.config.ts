7ffa0d331a325da4ec5c5b8287192476c7b1e84d
KulichO <oaosokina@yandex.ru>
2025-11-25 13:13:54 +0300

    commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/3.2.3_Kulikova_Olga/",
+  build: {
+    outDir: "docs",
+  },
   test: {
     environment: "jsdom",
     globals: true,
